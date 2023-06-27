97c48a4499d5e8c4b82be1037df50df7fe5a07ab
Miguel <122368297+27mfp@users.noreply.github.com>
2023-06-27 11:35:04 +0100

    updated svg icon

--- src/components/AddReply.tsx
+++ src/components/AddReply.tsx
@@ -101,12 +101,11 @@ const AddReply: React.FC<AddReplyProps> = ({ commentId, onReplyAdded }) => {
             width='16'
             height='16'
             fill='currentColor'
-            className='bi bi-arrow-bar-up'
+            className='bi bi-upload'
             viewBox='0 0 16 16'>
-            <path
-              fill-rule='evenodd'
-              d='M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5zm-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5z'
-            />{" "}
+            {" "}
+            <path d='M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z' />{" "}
+            <path d='M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z' />{" "}
           </svg>
         </label>
         <input
