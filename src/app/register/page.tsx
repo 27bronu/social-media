6b1811364ecc897d26e9a51883066a3cb400cda3
Bronu <you@example.com>
2023-06-28 10:21:43 +0100

    feature: added a loading screen between pages

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -15,7 +15,13 @@ export default function Register() {
   const [showPassword, setShowPassword] = useState(false);
   const [popUpMessage, setPopUpMessage] = useState("");
   const [showPopUp, setShowPopUp] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
+
+  const Login = () => {
+    setIsLoading(true);
+    router.push("./login");
+  };
 
   function showPopUpMessage(message: string) {
     setPopUpMessage(message);
@@ -86,94 +92,123 @@ export default function Register() {
   return (
     <main>
       {showPopUp && (
-        <div className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70'>
-          <div className='bg-white p-8 rounded-lg'>
-            <h2 className='text-2xl font-bold text-blue-900 mb-4'>ALERT</h2>
-            <p className='text-lg'>{popUpMessage}</p>
-            <div className='flex justify-center mt-8'>
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70">
+          <div className="bg-white p-8 rounded-lg">
+            <h2 className="text-2xl font-bold text-blue-900 mb-4">ALERT</h2>
+            <p className="text-lg">{popUpMessage}</p>
+            <div className="flex justify-center mt-8">
               <button
-                className='bg-blue-900 hover:bg-blue-900 text-white font-bold py-2 px-6 rounded'
-                onClick={closePopUp}>
+                className="bg-blue-900 hover:bg-blue-900 text-white font-bold py-2 px-6 rounded"
+                onClick={closePopUp}
+              >
                 Close
               </button>
             </div>
           </div>
         </div>
       )}
-
-      <div className='flex flex-col justify-center items-center h-screen bg-gray-900 text-white'>
-        <div className='bg-gray-500 text-black px-6 py-4 rounded-lg'>
-          <div className='box-border h-4/5 mt-1 w-72 p-4 border-1 border-blue-900 shadow-2xl flex flex-col justify-start items-center mb-4 bg-white rounded-lg'>
-            <Image src='/logo.png' width={200} height={100} alt='' />
-            <label className='mt-4 text-sm'>Email</label>
-            <input
-              type='email'
-              id='email'
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-              className='mt-2 p-2 border-2 border-blue-900'
-              required
-            />
-            <label className='mt-4 text-sm'>Username</label>
-            <input
-              type='text'
-              id='username'
-              value={username}
-              onChange={(e) => setUsername(e.currentTarget.value)}
-              className='mt-2 p-2 border-2 border-blue-900'
-              minLength={3}
-              required
-            />
-            <label className='mt-4 text-sm'>Name</label>
-            <input
-              type='text'
-              id='name'
-              value={name}
-              onChange={(e) => setName(e.currentTarget.value)}
-              className='mt-2 p-2 border-2 border-blue-900'
-            />
-            <label className='mt-4 text-sm'>Password</label>
-            <div className='relative flex items-center'>
-              <input
-                type={showPassword ? "text" : "password"}
-                id='password'
-                value={password}
-                onChange={(e) => setPassword(e.currentTarget.value)}
-                className='mt-2 p-2 border-2 border-blue-900'
-                minLength={5}
-                required
+      {isLoading ? (
+        <div className="flex items-center justify-center min-h-screen bg-gray-900">
+          <div className="flex items-center">
+            <span className="text-white">Loading</span>
+            <svg
+              aria-hidden="true"
+              className="w-8 h-8 ml-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
               />
-              <span
-                className='absolute right-2 text-lg cursor-pointer top-7 transform -translate-y-1/2'
-                style={{
-                  margin: "0 0.5rem",
-                }}
-                onClick={togglePasswordVisibility}>
-                {showPassword ? (
-                  <BsEyeSlash className='text-lg' />
-                ) : (
-                  <BsEye className='text-lg' />
-                )}
-              </span>
-            </div>
-            <button
-              onClick={addUser}
-              className='mt-4 mb-4 bg-blue-900 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded'>
-              Continue
-            </button>
-          </div>
-          <div className='box-border h-20 mt-8 w-72 p-4 border-1 border-blue-500 shadow-2xl flex flex-col justify-start items-center bg-white rounded-lg'>
-            <h1 className='mt-4 text-sm'>
-              <div>
-                Already have an account? ‎
-                <Link className='underline text-blue-900' href='/login'>
-                  Log in.
-                </Link>
-              </div>
-            </h1>
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
           </div>
         </div>
-      </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center h-screen bg-gray-900 text-white">
+          <div className="bg-gray-500 text-black px-6 py-4 rounded-lg">
+            <div className="box-border h-4/5 mt-1 w-72 p-4 border-1 border-blue-900 shadow-2xl flex flex-col justify-start items-center mb-4 bg-white rounded-lg">
+              <Image src="/logo.png" width={200} height={100} alt="" />
+              <label className="mt-4 text-sm">Email</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.currentTarget.value)}
+                className="mt-2 p-2 border-2 border-blue-900"
+                required
+              />
+              <label className="mt-4 text-sm">Username</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.currentTarget.value)}
+                className="mt-2 p-2 border-2 border-blue-900"
+                minLength={3}
+                required
+              />
+              <label className="mt-4 text-sm">Name</label>
+              <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={(e) => setName(e.currentTarget.value)}
+                className="mt-2 p-2 border-2 border-blue-900"
+              />
+              <label className="mt-4 text-sm">Password</label>
+              <div className="relative flex items-center">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.currentTarget.value)}
+                  className="mt-2 p-2 border-2 border-blue-900"
+                  minLength={5}
+                  required
+                />
+                <span
+                  className="absolute right-2 text-lg cursor-pointer top-7 transform -translate-y-1/2"
+                  style={{
+                    margin: "0 0.5rem",
+                  }}
+                  onClick={togglePasswordVisibility}
+                >
+                  {showPassword ? (
+                    <BsEyeSlash className="text-lg" />
+                  ) : (
+                    <BsEye className="text-lg" />
+                  )}
+                </span>
+              </div>
+              <button
+                onClick={addUser}
+                className="mt-4 mb-4 bg-blue-900 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded"
+              >
+                Continue
+              </button>
+            </div>
+            <div className="box-border h-20 mt-8 w-72 p-4 border-1 border-blue-500 shadow-2xl flex flex-col justify-start items-center bg-white rounded-lg">
+              <h1 className="mt-4 text-sm">
+                <div>
+                  Already have an account? ‎
+                  <a
+                    className="underline text-blue-900 cursor-pointer"
+                    onClick={Login}
+                  >
+                    Log in.
+                  </a>
+                </div>
+              </h1>
+            </div>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
